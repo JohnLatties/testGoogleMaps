1594dd2632accba2cde542b2c8b6c8e793c373d1
Jonathan Latties <jon.latties@gmail.com>
2016-02-15 23:49:28 -0200

    Testing APIGoogleMaps - Geolocation

---
+++ maps.js
@@ -0,0 +1,12 @@
+var mapGoogle = function initMap(){
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -30.108, lng : -51.176},
+      zoom: 10  
+    });
+    
+    
+    
+    //Try HTML5 geolocation
+    
+    
+}
